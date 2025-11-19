96dd3958452fc4c916cfa82ea4becf8688c32114
Elej97 <elifcaliskaner@hotmail.com>
2025-11-19 17:31:56 +0300

    Create db.php
    add database connection file.

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "fitness_system";
+$conn = mysqli_connect($host, $user,$pass,$db);
+
+if(!conn){
+    ("Database connection failed:" . mysqşi_connect_error());
+
+}
+?>
